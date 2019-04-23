d2cd4bea74cb76bd5cba1922338453e6ee955988
Mercykylax <nyawiramercy49@gmail.com>
2019-04-23 15:03:15 +0300

    my mpesa code

---
+++ mpesa.py
@@ -0,0 +1,22 @@
+class Mpesaaccount:
+	def__init__(self,name,phone__n.o,balance)
+	               self.name=name
+	               self.phone__n.o=phone__n.o
+	               self.number=number
+	def deposit(self,y):
+		deposit = y
+		self.balance = self.balance + y
+		info 1="Hello {},confirmed you have deposited {} kshs in your account.your new Mpesa balance is{},kshs".format(self.name,y,self.balance)
+		print(info 1)
+	def withdraw(self,x):
+		x<self.balance
+		self.balance=self.balance - x
+		info 2 = "Hello {},confirmed your withdrawal of {}kshs is succesful.your new mpesa balance is {}kshs".format(self.name,x,self.balance)
+		print(info2)
+	def balance(self):
+	    info 3 = "Hello {},your current balance is{},".format(self.name,self.balance)
+	    print(info 3)	
+		
+
+
+	                   
